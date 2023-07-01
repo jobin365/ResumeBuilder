3d80ae3634db58362ca172a07076f75edcb10576
Jobin John K <jobinjohnk5@gmail.com>
2023-07-01 15:49:00 +0530

    use react-pdf instead of js-pdf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { useEffect, useRef, useState } from "react";
 import Divider from "@mui/joy/Divider";
 import Button from "@mui/joy/Button";
-import jsPDF from "jspdf";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import GoogleIcon from "@mui/icons-material/Google";
@@ -11,10 +10,22 @@ import DownloadIcon from "@mui/icons-material/Download";
 import SaveIcon from "@mui/icons-material/Save";
 import LogoutIcon from "@mui/icons-material/Logout";
 import LoadingBar from "react-top-loading-bar";
-import { flushSync } from "react-dom";
 import Axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import Chip from "@mui/joy/Chip";
+import emailIcon from "./images/email.png";
+import githubIcon from "./images/github.png";
+import linkedinIcon from "./images/linkedin.png";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFDownloadLink,
+  Image,
+  Font,
+} from "@react-pdf/renderer";
 
 function App() {
   const [username, setUserName] = useState("");
@@ -38,9 +49,11 @@ function App() {
   const [education, setEducation] = useState([]);
   const [certifications, setCerts] = useState([]);
 
-  const prod=false;
+  const prod = false;
 
-  Axios.defaults.baseURL = prod?"https://resume-builder-sl0y.onrender.com":"http://localhost:3001";
+  Axios.defaults.baseURL = prod
+    ? "https://resume-builder-sl0y.onrender.com"
+    : "http://localhost:3001";
   Axios.defaults.withCredentials = true;
 
   useEffect(() => {
@@ -69,27 +82,6 @@ function App() {
       setCerts(resume.certifications);
     });
   }, []);
-
-  const handleGeneratePdf = () => {
-    bar.current.continuousStart();
-    if (w < 1330)
-      flushSync(() => {
-        setVisible(true);
-      });
-    const doc = new jsPDF({
-      format: "a4",
-      unit: "px",
-      hotfixes: ["px_scaling"],
-    });
-    doc.html(resume.current, {
-      async callback(doc) {
-        await doc.save("Resume");
-        if (w < 1330) setVisible(false);
-        bar.current.complete();
-      },
-      autoPaging: "text",
-    });
-  };
 
   const handleExperienceChange = (event) => {
     const { name, value } = event.target;
@@ -266,6 +258,138 @@ function App() {
     });
   };
 
+  Font.register({
+    family: "Open Sans",
+    fonts: [
+      {
+        src: "https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-regular.ttf",
+      },
+      {
+        src: "https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-600.ttf",
+        fontWeight: 600,
+      },
+    ],
+  });
+
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: "row",
+      fontFamily: "Open Sans",
+    },
+    section: {
+      flexGrow: 1,
+      margin: 10,
+      padding: 10,
+    },
+    bigHeading: {
+      fontSize: 25,
+      fontWeight: 600,
+    },
+    belowHeading: {
+      fontSize: 13,
+    },
+    subHeading: {
+      fontSize: 18,
+      marginTop: 10,
+      fontWeight: 600,
+    },
+    paragraph: {
+      fontSize: 11,
+      marginTop: 2,
+      textAlign: "justify",
+    },
+    textwithicon: {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "row",
+    },
+    title: {
+      fontSize: 13,
+      fontWeight: 600,
+    },
+  });
+
+  const MyDocument = (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={[styles.section, { width: "50%", paddingRight: 0 }]}>
+          <Text style={styles.bigHeading}>{name}</Text>
+          <Text style={styles.belowHeading}>{designation}</Text>
+
+          <View style={[styles.textwithicon, { marginTop: 10 }]}>
+            <Image
+              src={emailIcon}
+              style={{ width: 15, marginRight: 5 }}
+            ></Image>
+            <Text style={[styles.paragraph]}>{email}</Text>
+          </View>
+
+          <View style={[styles.textwithicon, { marginTop: 2 }]}>
+            <Image
+              src={linkedinIcon}
+              style={{ width: 15, marginRight: 5 }}
+            ></Image>
+            <Text style={[styles.paragraph]}>{linkedin}</Text>
+          </View>
+
+          <View style={[styles.textwithicon, { marginTop: 2 }]}>
+            <Image
+              src={githubIcon}
+              style={{ width: 15, marginRight: 5 }}
+            ></Image>
+            <Text style={[styles.paragraph]}>{github}</Text>
+          </View>
+
+          <Text style={[styles.subHeading]}>Summary</Text>
+          <Text style={[styles.paragraph, {}]}>{summary}</Text>
+
+          <Text style={[styles.subHeading]}>Experience</Text>
+          {experience.map((experience, i) => (
+            <>
+              <Text style={[styles.title, { marginTop: i === 0 ? 0 : 7 }]}>
+                {experience.designation}
+              </Text>
+              <Text style={[styles.paragraph]}>{experience.company}</Text>
+              <Text style={[styles.paragraph]}>{experience.duration}</Text>
+            </>
+          ))}
+
+          <Text style={[styles.subHeading]}>Education</Text>
+          {education.map((education, i) => (
+            <>
+              <Text style={[styles.title, { marginTop: i === 0 ? 0 : 7 }]}>
+                {education.degree}
+              </Text>
+              <Text style={[styles.paragraph]}>{education.institute}</Text>
+              <Text style={[styles.paragraph]}>{education.duration}</Text>
+            </>
+          ))}
+        </View>
+
+        <View style={[styles.section, { width: "50%", paddingLeft: 0 }]}>
+          <Text style={[styles.subHeading, { marginTop: 0 }]}>Skills</Text>
+          <Text style={[styles.paragraph]}>{skills.replace(/,/g, " • ")}</Text>
+          <Text style={[styles.subHeading]}>Certifications</Text>
+          {certifications.map((cert, i) => (
+            <Text style={[styles.paragraph]}>{"\u2022 " + cert.title}</Text>
+          ))}
+          <Text style={[styles.subHeading]}>Projects</Text>
+          {projects.map((project, i) => (
+            <>
+              <Text style={[styles.title, { marginTop: i === 0 ? 0 : 7 }]}>
+                {project.title}
+              </Text>
+              <Text style={[styles.paragraph]}>
+                {project.skills.replace(/,/g, " • ")}
+              </Text>
+              <Text style={[styles.paragraph]}>{project.desc}</Text>
+            </>
+          ))}
+        </View>
+      </Page>
+    </Document>
+  );
+
   return (
     <div style={{ fontFamily: "Helvetica" }}>
       <LoadingBar color="#f11946" ref={bar} />
@@ -299,13 +423,26 @@ function App() {
                   >
                     Save
                   </Button>
-                  <Button
-                    startDecorator={<DownloadIcon />}
-                    style={{ marginLeft: "10px" }}
-                    onClick={handleGeneratePdf}
-                  >
-                    Download
-                  </Button>
+                  <PDFDownloadLink document={MyDocument} fileName="Resume.pdf">
+                    {({ blob, url, loading, error }) =>
+                      loading ? (
+                        <Button
+                          startDecorator={<DownloadIcon />}
+                          style={{ marginLeft: "10px" }}
+                        >
+                          Loading
+                        </Button>
+                      ) : (
+                        <Button
+                          startDecorator={<DownloadIcon />}
+                          style={{ marginLeft: "10px" }}
+                        >
+                          Download
+                        </Button>
+                      )
+                    }
+                  </PDFDownloadLink>
+
                   <Button
                     onClick={logout}
                     color="danger"
@@ -325,17 +462,33 @@ function App() {
                       color="success"
                       variant="solid"
                       onClick={saveResume}
-                      style={{ marginLeft:w<=580? "0px":"10px" }}
+                      style={{ marginLeft: w <= 580 ? "0px" : "10px" }}
                     >
                       <SaveIcon />
                     </IconButton>
-                    <IconButton
-                      style={{ marginLeft: "10px" }}
-                      variant="solid"
-                      onClick={handleGeneratePdf}
+                    <PDFDownloadLink
+                      document={MyDocument}
+                      fileName="Resume.pdf"
                     >
-                      <DownloadIcon />
-                    </IconButton>
+                      {({ blob, url, loading, error }) =>
+                        loading ? (
+                          <IconButton
+                            style={{ marginLeft: "10px" }}
+                            variant="solid"
+                          >
+                            <DownloadIcon />
+                          </IconButton>
+                        ) : (
+                          <IconButton
+                            style={{ marginLeft: "10px" }}
+                            variant="solid"
+                          >
+                            <DownloadIcon />
+                          </IconButton>
+                        )
+                      }
+                    </PDFDownloadLink>
+
                     <IconButton
                       style={{ marginLeft: "10px" }}
                       color="danger"
@@ -348,7 +501,13 @@ function App() {
                 </>
               )
             ) : (
-              <a href={prod?"https://resume-builder-sl0y.onrender.com/auth/google":"http://localhost:3001/auth/google"}>
+              <a
+                href={
+                  prod
+                    ? "https://resume-builder-sl0y.onrender.com/auth/google"
+                    : "http://localhost:3001/auth/google"
+                }
+              >
                 <Button startDecorator={<GoogleIcon />}>
                   {w >= 640 ? "Login with Google" : "Login"}
                 </Button>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,291 +1,201 @@
 import "./Home.css";
 import Input from "@mui/joy/Input";
-import Divider from "@mui/joy/Divider";
 import Textarea from "@mui/joy/Textarea";
-import emailIcon from "../images/email.png";
-import githubIcon from "../images/github.png";
-import linkedinIcon from "../images/linkedin.png";
-import Online from "./Online";
-import History from "./History";
-import Project from "./Project";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@mui/joy/IconButton";
-
 export default function Home(props) {
   return (
-    <>
-      <div className="editor" style={{ width: "500px", margin: "25px" }}>
-        <h3 style={{ marginTop: "0px" }}>About</h3>
-        <br />
-        <Input
-          placeholder="Name"
-          value={props.name}
-          onChange={(e) => props.setName(e.target.value)}
-          name="name"
-        />
-        <br />
-        <Input
-          name="designation"
-          placeholder="Designation"
-          value={props.designation}
-          onChange={(e) => props.setDesignation(e.target.value)}
-        />
-        <br />
-        <Input
-          name="email"
-          placeholder="Email address"
-          value={props.email}
-          onChange={(e) => props.setEmail(e.target.value)}
-        />
-        <br />
-        <Input
-          name="linkedin"
-          placeholder="Linkedin profile URL"
-          value={props.linkedin}
-          onChange={(e) => props.setLinkedIn(e.target.value)}
-        />
-        <br />
-        <Input
-          name="github"
-          placeholder="GitHub profile URL"
-          value={props.github}
-          onChange={(e) => props.setGitHub(e.target.value)}
-        />
-        <br />
-        <Textarea
-          placeholder="Skill separated by comma without space"
-          value={props.skills}
-          onChange={(e) => props.setSkills(e.target.value)}
-          minRows={3}
-          name="skills"
-        />
-        <br />
-        <Textarea
-          placeholder="Summary"
-          value={props.summary}
-          onChange={(e) => props.setSummary(e.target.value)}
-          minRows={3}
-          name="summary"
-        />
-        <br />
-        <div className="editor-heading">
-          <h3 style={{ marginTop: "0px" }}>Certifications</h3>
-          <IconButton variant="solid" onClick={props.handleCertAdd}>
-            <AddIcon />
-          </IconButton>
-        </div>
-        <br />
-        {props.certifications.map((cert, index) => (
-          <div key={cert.id} id={cert.id} onChange={props.handleCertChange}>
-            <div className="editor-heading">
-              <Input
-                name="title"
-                placeholder={"Certification name"}
-                value={cert.title}
-                style={{ width: "100%", marginRight: "20px" }}
-              />
-              <IconButton color="danger" onClick={props.handleCertDelete}>
-                <DeleteIcon />
-              </IconButton>
-            </div>
-            <br />
-          </div>
-        ))}
-        <div className="editor-heading">
-          <h3 style={{ marginTop: "0px" }}>Experience</h3>
-          <IconButton variant="solid" onClick={props.handleExperienceAdd}>
-            <AddIcon />
-          </IconButton>
-        </div>
-
-        <br />
-        {props.experience.map((experience, index) => (
-          <div
-            key={experience.id}
-            id={experience.id}
-            onChange={props.handleExperienceChange}
-          >
-            <div className="editor-heading">
-              <Input
-                name="company"
-                placeholder={"Company name"}
-                value={experience.company}
-                style={{ width: "100%", marginRight: "20px" }}
-              />
-              <IconButton color="danger" onClick={props.handleExperienceDelete}>
-                <DeleteIcon />
-              </IconButton>
-            </div>
-            <br />
-            <Input
-              name="designation"
-              placeholder={"Designation"}
-              value={experience.designation}
-            />
-            <br />
-            <Input
-              name="duration"
-              placeholder={"Duration"}
-              value={experience.duration}
-            />
-            <br />
-            <br/>
-          </div>
-        ))}
-        <div className="editor-heading">
-          <h3 style={{ marginTop: "0px" }}>Projects</h3>
-          <IconButton variant="solid" onClick={props.handleProjectAdd}>
-            <AddIcon />
-          </IconButton>
-        </div>
-        <br />
-        {props.projects.map((project, index) => (
-          <div
-            key={project.id}
-            id={project.id}
-            onChange={props.handleProjectChange}
-          >
-            <div className="editor-heading">
-              <Input
-                name="title"
-                placeholder={"Title"}
-                value={project.title}
-                style={{ width: "100%", marginRight: "20px" }}
-              />
-              <IconButton color="danger" onClick={props.handleProjectDelete}>
-                <DeleteIcon />
-              </IconButton>
-            </div>
-            <br />
-            <Input
-              name="skills"
-              placeholder={"Skills"}
-              value={project.skills}
-            />
-            <br />
-            <Input
-              name="desc"
-              placeholder={"Description"}
-              value={project.desc}
-            />
-            <br />
-            <br/>
-          </div>
-        ))}
-        <div className="editor-heading">
-          <h3 style={{ marginTop: "0px" }}>Education</h3>
-          <IconButton variant="solid" onClick={props.handleEducationAdd}>
-            <AddIcon />
-          </IconButton>
-        </div>
-        <br />
-        {props.education.map((education, index) => (
-          <div
-            key={education.id}
-            id={education.id}
-            onChange={props.handleEducationChange}
-          >
-            <div className="editor-heading">
-              <Input
-                name="degree"
-                placeholder={"Degree"}
-                value={education.degree}
-                style={{ width: "100%", marginRight: "20px" }}
-              />
-              <IconButton color="danger" onClick={props.handleEducationDelete}>
-                <DeleteIcon />
-              </IconButton>
-            </div>
-            <br />
-            <Input
-              name="institute"
-              placeholder={"Institute"}
-              value={education.institute}
-            />
-            <br />
-            <Input
-              name="duration"
-              placeholder={"Duration"}
-              value={education.duration}
-            />
-            <br />
-            <br/>
-          </div>
-        ))}
+    <div className="editor" style={{ width: "500px", margin: "25px" }}>
+      <h3 style={{ marginTop: "0px" }}>About</h3>
+      <br />
+      <Input
+        placeholder="Name"
+        value={props.name}
+        onChange={(e) => props.setName(e.target.value)}
+        name="name"
+      />
+      <br />
+      <Input
+        name="designation"
+        placeholder="Designation"
+        value={props.designation}
+        onChange={(e) => props.setDesignation(e.target.value)}
+      />
+      <br />
+      <Input
+        name="email"
+        placeholder="Email address"
+        value={props.email}
+        onChange={(e) => props.setEmail(e.target.value)}
+      />
+      <br />
+      <Input
+        name="linkedin"
+        placeholder="Linkedin profile URL"
+        value={props.linkedin}
+        onChange={(e) => props.setLinkedIn(e.target.value)}
+      />
+      <br />
+      <Input
+        name="github"
+        placeholder="GitHub profile URL"
+        value={props.github}
+        onChange={(e) => props.setGitHub(e.target.value)}
+      />
+      <br />
+      <Textarea
+        placeholder="Skill separated by comma without space"
+        value={props.skills}
+        onChange={(e) => props.setSkills(e.target.value)}
+        minRows={3}
+        name="skills"
+      />
+      <br />
+      <Textarea
+        placeholder="Summary"
+        value={props.summary}
+        onChange={(e) => props.setSummary(e.target.value)}
+        minRows={3}
+        name="summary"
+      />
+      <br />
+      <div className="editor-heading">
+        <h3 style={{ marginTop: "0px" }}>Certifications</h3>
+        <IconButton variant="solid" onClick={props.handleCertAdd}>
+          <AddIcon />
+        </IconButton>
       </div>
-      {props.w >= 1330 && <Divider orientation="vertical" />}
-      {(props.w >= 1330 || props.visible) && (
-        <div
-          className="resume"
-          style={{
-            fontFamily: "Helvetica,Times-Roman,Courier",
-          }}
-          ref={props.resume}
-        >
-          <div
-            style={{
-              width: "350px",
-              padding: "25px",
-              paddingRight: "0px",
-            }}
-          >
-            <h1 style={{ marginBottom: "0px", marginTop: "0px" }}>
-              {props.name}
-            </h1>
-            <h3 style={{ marginTop: "5px", marginBottom: "15px" }}>
-              {props.designation}
-            </h3>
-            <Online img={emailIcon} url={props.email} />
-            <Online img={linkedinIcon} url={props.linkedin} />
-            <Online img={githubIcon} url={props.github} />
-            <h2>Summary</h2>
-            <p>{props.summary}</p>
-            <h2>Experience</h2>
-            {props.experience.map((experience) => (
-              <History
-                key={experience.id}
-                title={experience.designation}
-                place={experience.company}
-                duration={experience.duration}
-              />
-            ))}
-            <h2>Education</h2>
-            {props.education.map((education) => (
-              <History
-                key={education.id}
-                title={education.degree}
-                place={education.institute}
-                duration={education.duration}
-              />
-            ))}
+      <br />
+      {props.certifications.map((cert, index) => (
+        <div key={cert.id} id={cert.id} onChange={props.handleCertChange}>
+          <div className="editor-heading">
+            <Input
+              name="title"
+              placeholder={"Certification name"}
+              value={cert.title}
+              style={{ width: "100%", marginRight: "20px" }}
+            />
+            <IconButton color="danger" onClick={props.handleCertDelete}>
+              <DeleteIcon />
+            </IconButton>
           </div>
-          <div
-            style={{
-              width: "350px",
-              padding: "25px",
-            }}
-          >
-            <h2 style={{ marginTop: "0px" }}>Skills</h2>
-            <p>{props.skills.replace(/,/g, " • ")}</p>
-            <h2>Certifications</h2>
-            <ul>
-              {props.certifications.map((cert) => (
-                <li key={cert.id}>{cert.title}</li>
-              ))}
-            </ul>
-            <h2>Projects</h2>
-            {props.projects.map((project) => (
-              <Project
-                key={project.id}
-                title={project.title}
-                tools={project.skills}
-                desc={project.desc}
-              />
-            ))}
-          </div>
+          <br />
         </div>
-      )}
-    </>
+      ))}
+      <div className="editor-heading">
+        <h3 style={{ marginTop: "0px" }}>Experience</h3>
+        <IconButton variant="solid" onClick={props.handleExperienceAdd}>
+          <AddIcon />
+        </IconButton>
+      </div>
+
+      <br />
+      {props.experience.map((experience, index) => (
+        <div
+          key={experience.id}
+          id={experience.id}
+          onChange={props.handleExperienceChange}
+        >
+          <div className="editor-heading">
+            <Input
+              name="company"
+              placeholder={"Company name"}
+              value={experience.company}
+              style={{ width: "100%", marginRight: "20px" }}
+            />
+            <IconButton color="danger" onClick={props.handleExperienceDelete}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+          <br />
+          <Input
+            name="designation"
+            placeholder={"Designation"}
+            value={experience.designation}
+          />
+          <br />
+          <Input
+            name="duration"
+            placeholder={"Duration"}
+            value={experience.duration}
+          />
+          <br />
+          <br />
+        </div>
+      ))}
+      <div className="editor-heading">
+        <h3 style={{ marginTop: "0px" }}>Projects</h3>
+        <IconButton variant="solid" onClick={props.handleProjectAdd}>
+          <AddIcon />
+        </IconButton>
+      </div>
+      <br />
+      {props.projects.map((project, index) => (
+        <div
+          key={project.id}
+          id={project.id}
+          onChange={props.handleProjectChange}
+        >
+          <div className="editor-heading">
+            <Input
+              name="title"
+              placeholder={"Title"}
+              value={project.title}
+              style={{ width: "100%", marginRight: "20px" }}
+            />
+            <IconButton color="danger" onClick={props.handleProjectDelete}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+          <br />
+          <Input name="skills" placeholder={"Skills"} value={project.skills} />
+          <br />
+          <Input name="desc" placeholder={"Description"} value={project.desc} />
+          <br />
+          <br />
+        </div>
+      ))}
+      <div className="editor-heading">
+        <h3 style={{ marginTop: "0px" }}>Education</h3>
+        <IconButton variant="solid" onClick={props.handleEducationAdd}>
+          <AddIcon />
+        </IconButton>
+      </div>
+      <br />
+      {props.education.map((education, index) => (
+        <div
+          key={education.id}
+          id={education.id}
+          onChange={props.handleEducationChange}
+        >
+          <div className="editor-heading">
+            <Input
+              name="degree"
+              placeholder={"Degree"}
+              value={education.degree}
+              style={{ width: "100%", marginRight: "20px" }}
+            />
+            <IconButton color="danger" onClick={props.handleEducationDelete}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+          <br />
+          <Input
+            name="institute"
+            placeholder={"Institute"}
+            value={education.institute}
+          />
+          <br />
+          <Input
+            name="duration"
+            placeholder={"Duration"}
+            value={education.duration}
+          />
+          <br />
+          <br />
+        </div>
+      ))}
+    </div>
   );
 }
